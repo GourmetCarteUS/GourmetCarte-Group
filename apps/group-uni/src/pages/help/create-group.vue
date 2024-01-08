e6255ca992676e530c15b44b67593d9aab120da9
lize <lize240810@163.com>
2024-01-08 14:17:47 +0800

    help fix

--- apps/group-uni/src/pages/help/create-group.vue
+++ apps/group-uni/src/pages/help/create-group.vue
@@ -16,6 +16,17 @@
         <uni-group title="5.进入群聊后即可发现群聊中只有自己一个成员，一个人的群聊建立成功。" mode="card">
             <image :src="createGroupImg5" class="h-full w-full" mode="widthFix" />
         </uni-group>
+        <uni-group title="6.点击“群二维码”生成二维码，点击“保存到通讯录”防止群丢失。" mode="card">
+            <image :src="createGroupImg6" class="h-full w-full" mode="widthFix" />
+        </uni-group>
+        <uni-group mode="card">
+            <template #title>
+                <view class="text-28" style="color: #666; padding: 20rpx 30rpx; background-color: #eee">
+                    7.点击“保存二维码”，可以获取群的二维码，<text class="text-red font-500">二维码有效期只有7天</text>，请注意过期时间防止用户无法加入。
+                </view>
+            </template>
+            <image :src="createGroupImg7" class="h-full w-full" mode="widthFix" />
+        </uni-group>
     </view>
 </template>
 
@@ -28,4 +39,6 @@ import createGroupImg2 from '@/static/images/create-group-2.png';
 import createGroupImg3 from '@/static/images/create-group-3.png';
 import createGroupImg4 from '@/static/images/create-group-4.png';
 import createGroupImg5 from '@/static/images/create-group-5.png';
+import createGroupImg6 from '@/static/images/create-group-6.png';
+import createGroupImg7 from '@/static/images/create-group-7.png';
 </script>
